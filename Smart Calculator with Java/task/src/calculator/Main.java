1ba341a2a30d170a81e2a80c2e7d8403e2248de6
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-03-26 21:35:44 -0400

    check protected classes problem

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -6,11 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println(
                 Integer.parseInt(
                         scanner.next()) + Integer.parseInt(scanner.next()
                 )
         );
         scanner.close();
+    }
+
+
+}
+
+class A {
+
+    private int a;
+
+    public void invokeB() {
+        B objB = new B(); // (1)
+        int b = objB.b;   // (2)
+        int c = objB.c;   // (3)
+    }
+}
+
+public class B {
+
+    private int b;
+    int c;
+
+    public void invokeA() {
+        A objA = new A(); // (4)
+        int a = objA.a;   // (5)
     }
 }
