5c6bd38a62bd9fe485f3888c88a2e5f06a93a4ce
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-03-28 22:07:57 -0400

    check protected classes problem

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -18,24 +18,4 @@ public class Main {
 
 }
 
-class A {
 
-    private int a;
-
-    public void invokeB() {
-        B objB = new B(); // (1)
-        int b = objB.b;   // (2)
-        int c = objB.c;   // (3)
-    }
-}
-
-public class B {
-
-    private int b;
-    int c;
-
-    public void invokeA() {
-        A objA = new A(); // (4)
-        int a = objA.a;   // (5)
-    }
-}
